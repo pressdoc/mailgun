7b2e97bf2ef8cd8425ee4b67332ea16b09989f2f
Menno Wildeboer <ome.menno@gmail.com>
2016-03-22 13:11:53 +0100

    omg

--- lib/mailgun.rb
+++ lib/mailgun.rb
@@ -1,5 +1,6 @@
-require_relative 'mailgun/api/mail'
+require_relative 'mailgun/api/messages'
 require_relative 'mailgun/api'
 require_relative 'mailgun/client'
 require_relative 'mailgun/exception'
 require_relative 'mailgun/mail'
+require_relative 'mailgun/response'
